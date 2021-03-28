79ff55769890a6d658bcda8b30f97d2a71f6789f
Warodom WERAPUN <wwarodom@gmail.com>
2021-03-28 22:32:39 +0700

    Add cors()

--- index.js
+++ index.js
@@ -2,17 +2,21 @@
 const express = require('express'),
     app = express(),
     passport = require('passport'),
-    port = process.env.PORT || 80
+    port = process.env.PORT || 80,
+    cors = require('cors')
 
 const bcrypt = require('bcrypt')
 
-const db = require('./database.js');
+const db = require('./database.js')
 let users = db.users
 
 require('./passport.js')
 
 const router = require('express').Router(),
     jwt = require('jsonwebtoken')
+
+
+app.use(cors({ origin: 'http://localhost:3000' }));    
 
 app.use('/api', router)
 router.use(express.json())
